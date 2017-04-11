568f49068e15fdcbfcb4deb3af78bc24cd62895b
t1st3 <contact@tiste.org>
2017-04-11 17:09:46 +0200

    feat: (breaking) es2015ify and drop node 0.10 and 0.12
    * Bump deps
    * drop support for Node.js 0.10 and 0.12
    * use ES2015

--- test.js
+++ test.js
@@ -3,16 +3,10 @@ import readChunk from 'read-chunk';
 import test from 'ava';
 import isRpm from './';
 
-test('should detect RPM from buffer', function (t) {
-	readChunk(path.join(__dirname, 'fixture.rpm'), 0, 4, function (err, buf) {
-		t.is(err, null);
-		t.is(isRpm(buf), true);
-	});
+test('should detect RPM from buffer', t => {
+	t.is(isRpm(readChunk.sync(path.join(__dirname, 'fixture.rpm'), 0, 4)), true);
 });
 
-test('should not detect RPM on a non-RPM file', function (t) {
-	readChunk(path.join(__dirname, 'fixture.jpg'), 0, 4, function (err, buf) {
-		t.is(err, null);
-		t.is(isRpm(buf), false);
-	});
+test('should not detect RPM on a non-RPM file', t => {
+	t.is(isRpm(readChunk.sync(path.join(__dirname, 'fixture.jpg'), 0, 4)), false);
 });
